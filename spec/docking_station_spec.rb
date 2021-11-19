b44a2b42f7ecb1eaccceaa68cee17912d34ac677
Francesco <francescocollotto@gmail.com>
2021-11-19 16:54:09 +0000

    refactor to use doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,22 +1,22 @@
 require "boris-bikes"
 
 describe DockingStation do
+    let(:bike) { double(:bike) }
+    
     it "has default capacity" do
         expect(subject.capacity).to eq(DockingStation::DEFAULT_CAPACITY)
     end
     it "has variable capacity" do
         dock = DockingStation.new(50)
-        bike = Bike.new
-        dock.capacity.times { dock.return_bike Bike.new}
-        expect {dock.return_bike(bike) }.to raise_error("Dockingstation is full")
+        dock.capacity.times { dock.return_bike double(:bike)}
+        expect {dock.return_bike(double(:bike)) }.to raise_error("Dockingstation is full")
     end
     
     it { expect(DockingStation).to respond_to(:new).with(1).argument }
     it { expect(subject).to respond_to(:release_bike) }
     it { expect(subject).to respond_to(:return_bike).with(1).argument }
     it "return a bike to DockingStation" do
-        bike = Bike.new
-        expect(subject.return_bike(bike)).to be_an_instance_of Array
+        expect(subject.return_bike(double(:bike))).to be_an_instance_of Array
     end
     
     describe "#release_bike" do
@@ -24,27 +24,26 @@ describe DockingStation do
             expect { subject.release_bike }.to raise_error("Sorry no bikes available")
         end
         it "release a bike" do
-            bike = Bike.new
             docking_station = DockingStation.new
             docking_station.return_bike(bike)
-            expect(docking_station.release_bike).to be_an_instance_of Bike
+            allow(bike).to receive(:broken).and_return(false)
+            expect(docking_station.release_bike).to eq(bike)
         end
     end
 
     describe "#return_bike" do
         it "raises an error if DockingStation is full" do
-            bike = Bike.new
             dock = DockingStation.new
-            dock.capacity.times { dock.return_bike Bike.new }
-            expect { dock.return_bike(bike) }.to raise_error("Dockingstation is full")
+            dock.capacity.times { dock.return_bike double(:bike) }
+            expect { dock.return_bike(double(:bike)) }.to raise_error("Dockingstation is full")
         end
         it "add a bike if there is no bikes" do
-            bike = Bike.new
-            expect(subject.return_bike(bike)).to be_an_instance_of Array
+            expect(subject.return_bike(double(:bike))).to be_an_instance_of Array
         end
         it "return a bike if broken" do
-            bike = Bike.new
-            bike.report_broken
+            # bike = double(:bike)
+            # allow(bike).to receive(:report_broken).and_return(true)
+            # bike.report_broken
             subject.return_bike(bike)
             expect(subject.bikes.include?(bike)).to eq true
         end
